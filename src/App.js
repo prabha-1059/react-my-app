332da0b782b060cad46ea4dbc2b8f499b97f76ef
prabha-1059 <pry1059@gmail.com>
2021-01-25 15:49:05 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from "react";
 import "./style.css";
 
 export default function App() {
+  const sayHelloHan=()=>{
+    console.log("helo")
+  }
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <button onClick={sayHelloHan}> Click
+      </button>
     </div>
   );
 }
